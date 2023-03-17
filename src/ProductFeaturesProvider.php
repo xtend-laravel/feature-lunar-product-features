ad162af49ce264567ef37ece060a547bafc0ddf2
Adam Lee <adam@codelabx.ltd>
2023-03-17 15:55:26 +0000

    Register into sidebar menu

--- src/ProductFeaturesProvider.php
+++ src/ProductFeaturesProvider.php
@@ -2,15 +2,18 @@
 
 namespace XtendLunar\Features\ProductFeatures;
 
-use Illuminate\Support\ServiceProvider;
+use CodeLabX\XtendLaravel\Base\XtendFeatureProvider;
+use Illuminate\Foundation\Events\LocaleUpdated;
+use Illuminate\Support\Facades\Event;
 use Livewire\Livewire;
+use Lunar\Hub\Facades\Menu;
 use Lunar\Hub\Facades\Slot;
 use XtendLunar\Features\ProductFeatures\Livewire\Components\ProductFeatureEdit;
 use XtendLunar\Features\ProductFeatures\Livewire\Components\ProductFeatureValueEdit;
 use XtendLunar\Features\ProductFeatures\Livewire\Pages\FeaturesIndex;
 use XtendLunar\Features\ProductFeatures\Livewire\Slots\ProductFeatureSlot;
 
-class ProductFeaturesProvider extends ServiceProvider
+class ProductFeaturesProvider extends XtendFeatureProvider
 {
     public function register(): void
     {
@@ -26,5 +29,22 @@ class ProductFeaturesProvider extends ServiceProvider
         Livewire::component('hub.components.products.slots.product-feature-slot', ProductFeatureSlot::class);
 
         Slot::register('product.show', ProductFeatureSlot::class);
+
+        // @todo Move this to XtendFeatureProvider to check if method exists
+        $this->registerWithSidebarMenu();
+    }
+
+    protected function registerWithSidebarMenu(): void
+    {
+        // Note: We listen to LocaleUpdated event to make sure translations are loaded and menu items are all available
+        Event::listen(LocaleUpdated::class, function () {
+            Menu::slot('sidebar')->section('catalogue-manager')->addItem(function ($item) {
+                $item->name('Product Features')
+                     ->handle('hub.product-features')
+                     ->route('hub.product-features.index')
+                     ->gate('settings:core')
+                     ->icon('clipboard-list');
+            }, 'hub.products');
+        });
     }
 }
